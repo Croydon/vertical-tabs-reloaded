52deb583ef4718775477199faedcba19ff1a21ad
Croydon <cr0ydon@outlook.com>
2017-09-22 04:14:56 +0200

    WebExt: Rename lib.js to sidebar.js

--- webextension/sidebar.js
+++ webextension/sidebar.js
@@ -1,10 +1,3 @@
-/* ***** BEGIN LICENSE BLOCK *****
- *
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * ***** END LICENSE BLOCK ***** */
-
 "use strict";
 
 var main = browser.extension.getBackgroundPage();
@@ -87,7 +80,7 @@ var VerticalTabsReloaded = class VerticalTabsReloaded
     init()
     {
         this.debug_log(this.webExtPreferences);
-        this.window.VerticalTabsReloaded = this; // FIXME: Likely not needed anymore
+        // this.window.VerticalTabsReloaded = this; // FIXME: Likely not needed anymore
         // this.unloaders.push(() =>
         // {
         //     delete this.window.VerticalTabsReloaded;
