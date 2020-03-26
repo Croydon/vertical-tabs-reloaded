8c88a71f9c5c00df09c9a6fcdc73cc5f8822fd8a
Croydon <git@cr0ydon.com>
2020-03-26 14:24:00 +0100

    Add self-hosted dev version publishing (#241)

---
+++ dev/script/prepare_dev_release.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import json 
+import os 
+
+manifest = None
+manifestfile = os.path.join("manifest.json")
+
+with open(manifestfile, "r") as manifest_file:
+    manifest = json.load(manifest_file)
+
+with open(manifestfile, "w") as manifest_file:
+    manifest["name"] = "VTR (DEVELOPER VERSION)"
+    manifest["applications"]["gecko"]["id"] = "vtrbeta@go-dev.de"
+    manifest["applications"]["gecko"]["update_url"] = "https://croydon.github.io/vtr-releases/updates.json"
+
+    manifest = json.dumps(manifest, indent=4)
+    manifest_file.write("{}\n".format(manifest))
